1bfa831f3770b33a8f0cb6376f906eabba0fe403
Muyundo <muyundobrian@gmail.com>
2024-04-11 15:51:41 +0300

    updated random caregiver phone number

--- cypress/e2e/Chanjo-provider-web/register_client.cy.js
+++ cypress/e2e/Chanjo-provider-web/register_client.cy.js
@@ -6,18 +6,19 @@ context('Actions', () => {
         cy.login()
     })
   
-     
+    const randomNumberOfKeyPresses = Math.floor(Math.random() * 5) + 1; // Random number between 1 and 5
+
     it('.type() - Register a Client', () => {
         cy.get('.max-w-7xl > [href="/register-client/_"]').click()
-        cy.wait(1000)
+        cy.wait(2500)
         cy.get('#clientDetails_firstName').type('Jane')
         cy.get('#clientDetails_middleName').type('N')
         cy.get('#clientDetails_lastName').type('Newton')
         cy.get('#clientDetails_gender > :nth-child(2) > .ant-radio > .ant-radio-input').click()
-        cy.get('#clientDetails_estimatedAge > :nth-child(2) > .ant-radio > .ant-radio-input').click()
+       // cy.get('#clientDetails_estimatedAge > :nth-child(2) > .ant-radio > .ant-radio-input').click()
 
         //select a random date back 5 years
-       /* cy.get('.ant-picker-input').click()
+        cy.get('.ant-picker-input').click()
         cy.get('.ant-picker-cell:not(.ant-picker-cell-disabled)').should('exist').then($dateCells => {
         const numAvailableDates = $dateCells.length        
         const randomIndex = Math.floor(Math.random() * numAvailableDates)        
@@ -26,7 +27,7 @@ context('Actions', () => {
        // const [year, month, day] = dateTitle.split('-')
        // const formattedDate = `${day}-${month}-${year.slice(-2)}`        
         randomDateCell.click()
-*/
+
 
         const randomPresses11 = Math.floor(Math.random() * 0); // Change 2 to 3 to include the first option
 
@@ -44,24 +45,33 @@ context('Actions', () => {
         cy.get('#caregiverType').click()        
         cy.get('#caregiverType').trigger("keydown", { keyCode: 13 }); 
         cy.get('#caregiverName').type('Alex')
-        cy.get('#phoneNumber').type('0712345654')
+
+        const randomNumber2 =
+        "07" +
+        Math.floor(Math.random() * 1000000000)
+          .toString()
+          .padStart(8, "0");
+  
+          cy.get('#phoneNumber').type(randomNumber); // Phone number
+
         cy.get('.grid > .ml-4').click()
         cy.get('button').contains('Next').click();
+        cy.wait(1500)
+ 
+            cy.get('#headlessui-combobox-input-\\:rt\\:').type("BARINGO").wait(1000).type("{downarrow}").type('{Enter}');
+            cy.get('#headlessui-combobox-input-\\:r13\\:').type("BARINGO").wait(1000).type("{downarrow}").type('{Enter}');
+            cy.get('#headlessui-combobox-input-\\:r19\\:').type("E").wait(1000).type("{downarrow}").type('{Enter}');
+            cy.get('button').contains('Preview').click();
+            cy.get('button').contains('Submit').click();
+            cy.wait(1000)
+            cy.get('button').contains('Close').click();
 
-
-        const county = [
-            "BUNGOMA",
-          ];
-          const randomCounty =
-            county[Math.floor(Math.random() * county.length)];
-    
-          cy.get('#headlessui-combobox-input-\:rv\:').type(randomCounty, { force: true });
-          cy.contains(randomCounty).click();
-
+            
+   
 
 
         })
       
   })
- // })
+  })
   
